1c6b78ba4dde779ccbdbad5df9e25cd4fd3f3912
uycuhnt2467 <uycuhnt2467@gmail.com>
2021-01-25 21:34:49 +0800

    Update 48.

--- 最長不含重複字符串的長度.py
+++ 最長不含重複字符串的長度.py
@@ -30,12 +30,33 @@ class Solution:
                 prev -= 1
             dp[i] = max(dp[i-1], count)
         return dp[-1]
-    def longest_substring_without_duplication(self, strings):
+    
+    def longest_substring_without_duplication_2(self, strings):
         # 優化2:
-        pass
+        import string
+        cur_len = 0
+        max_len = 0
         
+        pos_dict = dict()
+        for i in string.ascii_lowercase:
+            pos_dict[i] = -1
+        
+        for j in range(len(strings)):
+            prev = pos_dict[strings[j]]
+            if prev < 0 or j - prev > cur_len:
+                cur_len += 1
+            else:
+                if cur_len > max_len:
+                    max_len = cur_len
+                cur_len = j - prev
+            pos_dict[strings[j]] = j
+        
+        if cur_len > max_len:
+            max_len = cur_len
+        return max_len
     
 
 sol = Solution()
 strings = "arabcacfr"
 print(sol.longest_substring_without_duplication(strings))
+print(sol.longest_substring_without_duplication_2(strings))
